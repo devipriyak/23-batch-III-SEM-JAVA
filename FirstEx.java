2073e75505fe40f5dee05ef0b7ae4a881fa0c959
Dr K DeviPriya <k.devipriya20@gmail.com>
2024-07-15 15:39:59 +0530

    Add files via upload

---
+++ FirstEx.java
@@ -0,0 +1,15 @@
+
+class FirstEx
+{
+public static void main(String args[])
+		{
+		int a=10,b=20;
+		int c=a+b;
+		String name="JAVA";
+		System.out.println("Hello");
+		System.out.println("CSE-A");
+		System.out.println("C is "+c);
+		System.out.println(a!=b);
+		System.out.println("Hello:"+name);
+		}
+}
