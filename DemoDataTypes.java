eafda880ee2bc25a93b8c458e5adbd6a96a889ac
Dr K DeviPriya <k.devipriya20@gmail.com>
2024-07-22 15:58:07 +0530

    Add files via upload

---
+++ DemoDataTypes.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+class DemoDataTypes
+{
+public static void main(String args[])
+{
+int rollnumber=1;
+String name="CSEA";
+char grade='A';
+double cgpa=9.2;
+long l=12203204950596096L;
+System.out.println("rollnumber"+rollnumber);
+System.out.println("name"+name);
+System.out.println("Grade"+grade);
+System.out.println("cgpa"+cgpa);
+System.out.println("long"+l);
+char ch1=68;
+System.out.println("char "+ch1);
+}
+}
+
